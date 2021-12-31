8171455e161f8fba1a1e1a82fa115c6751a1c959
Okechukwu Ikwunze <okechukwuikwunze@yahoo.com>
2021-12-31 16:13:39 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import cv2
+
+img=cv2.imread('galaxy.jpg',0)
+
+# print(type(img))
+# print(img)
+# print(img.shape)
+# print(img.ndim)
+
+cv2.imshow('galaxy', img)
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
---
+++ video_capture.py
@@ -0,0 +1,20 @@
+import cv2 
+import time
+
+video=cv2.VideoCapture(0)
+
+while True:
+    check, frame = video.read()
+
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # time.sleep(3)
+    cv2.imshow('Capturing',gray)
+
+    key=cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows
